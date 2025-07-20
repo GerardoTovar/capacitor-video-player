64e49633f5fe8ec8fe3764fdf95a533c7e209fca
Gerardo Emmanuel Tovar Barragan <gerardo.tovar@lestprototype.com>
2025-07-20 00:32:03 -0700

    test5

--- dist/plugin.js
+++ dist/plugin.js
@@ -1022,19 +1022,26 @@ var capacitorCapacitorVideoPlayer = (function (exports, core, Hls) {
         web: () => new CapacitorVideoPlayerWeb(),
     });
     const CapacitorVideoPlayer = Object.assign(Object.assign({}, NativeVideoPlayer), { initPlayer(options) {
-            // Si es embedded, usamos siempre la capa web para que participe del scroll
             if (options.mode === 'embedded') {
-                const web = new CapacitorVideoPlayerWeb();
-                return web.initPlayer(options);
+                return new CapacitorVideoPlayerWeb().initPlayer(options);
             }
-            // Si es fullscreen, llamamos al native
             return NativeVideoPlayer.initPlayer(options);
         },
-        removePlayer(options) {
-            // Igual: embedded lo limpia la capa web; fullscreen el native
+        play(options) {
             if (options.mode === 'embedded') {
-                const web = new CapacitorVideoPlayerWeb();
-                return web.removePlayer(options);
+                return new CapacitorVideoPlayerWeb().play(options);
+            }
+            return NativeVideoPlayer.play(options);
+        },
+        pause(options) {
+            if (options.mode === 'embedded') {
+                return new CapacitorVideoPlayerWeb().pause(options);
+            }
+            return NativeVideoPlayer.pause(options);
+        },
+        removePlayer(options) {
+            if (options.mode === 'embedded') {
+                return new CapacitorVideoPlayerWeb().removePlayer(options);
             }
             return NativeVideoPlayer.removePlayer(options);
         } });
--- src/index.ts
+++ src/index.ts
@@ -18,20 +18,29 @@ export const CapacitorVideoPlayer: CapacitorVideoPlayerPlugin = {
   ...NativeVideoPlayer,
 
   initPlayer(options: capVideoPlayerOptions): Promise<capVideoPlayerResult> {
-    // Si es embedded, usamos siempre la capa web para que participe del scroll
     if (options.mode === 'embedded') {
-      const web = new CapacitorVideoPlayerWeb();
-      return web.initPlayer(options);
+      return new CapacitorVideoPlayerWeb().initPlayer(options);
     }
-    // Si es fullscreen, llamamos al native
     return NativeVideoPlayer.initPlayer(options);
   },
 
-  removePlayer(options: capVideoPlayerIdOptions): Promise<capVideoPlayerResult> {
-    // Igual: embedded lo limpia la capa web; fullscreen el native
+  play(options: capVideoPlayerIdOptions): Promise<capVideoPlayerResult> {
     if ((options as any).mode === 'embedded') {
-      const web = new CapacitorVideoPlayerWeb();
-      return web.removePlayer(options);
+      return new CapacitorVideoPlayerWeb().play(options);
+    }
+    return NativeVideoPlayer.play(options);
+  },
+
+  pause(options: capVideoPlayerIdOptions): Promise<capVideoPlayerResult> {
+    if ((options as any).mode === 'embedded') {
+      return new CapacitorVideoPlayerWeb().pause(options);
+    }
+    return NativeVideoPlayer.pause(options);
+  },
+
+  removePlayer(options: capVideoPlayerIdOptions): Promise<capVideoPlayerResult> {
+    if ((options as any).mode === 'embedded') {
+      return new CapacitorVideoPlayerWeb().removePlayer(options);
     }
     return NativeVideoPlayer.removePlayer(options);
   },
