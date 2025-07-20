06d757e654c278a2b3d9ea362540d92266bdeff5
Gerardo Emmanuel Tovar Barragan <gerardo.tovar@lestprototype.com>
2025-07-19 23:37:55 -0700

    test2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { registerPlugin, Capacitor } from '@capacitor/core';
+import { registerPlugin } from '@capacitor/core';
 import type {
   CapacitorVideoPlayerPlugin,
   capVideoPlayerOptions,
